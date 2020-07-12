bf16617471e2ce56fe3dc1b5ae598d8a6e049a57
Stella Jung <sojung@myseneca.ca>
2020-07-11 22:28:57 -0400

    set up babel for node

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('뚱');
-});
-
-app.get('/star', (req, res) => {
-  res.send('* <br> ***');
-});
 
 app.listen(5000, () => {
   console.log(`server working @ http://localhost:5000`);
